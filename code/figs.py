e1f2264ab1946ca06f5331929398ed5e0f3dccaa
Elaine Angelino <elaine.angelino@gmail.com>
2016-04-26 17:27:08 -0700

    Improve figures about the cache

--- code/figs.py
+++ code/figs.py
@@ -8,28 +8,49 @@ def make_figure(froot, din, dout, max_accuracy, max_length, delimiter='\t'):
     fin = os.path.join(din, '%s.txt' % froot)
     x = tb.tabarray(SVfile=fin, delimiter=delimiter)
     pylab.ion()
-    pylab.figure(1, figsize=(10, 8))
-    pylab.clf()
-    nrows = max_length
-    ncols = 2
     for i in range(1, max_length + 1):
+        print i
+        pylab.figure(1, figsize=(14, 8))
+        pylab.clf()
         y = x[x['length'] == i]
-        pylab.subplot(nrows, ncols, (i - 1) * ncols + 1)
+        pylab.subplot(2, 3, 1)
         pylab.hist(y['accuracy'])
         pylab.xlabel('accuracy')
         a = list(pylab.axis())
         a[0] = 0.5
         a[1] = 1.
+        max_accuracy = y['accuracy'].max()
+        pylab.plot([max_accuracy, max_accuracy], [0, a[3]], 'r-')
         pylab.axis(a)
-        pylab.subplot(nrows, ncols, (i - 1) * ncols + 2)
+        pylab.subplot(2, 3, 2)
         pylab.hist(y['upper_bound'])
         pylab.xlabel('upper_bound')
         a = list(pylab.axis())
         a[0] = max_accuracy
         a[1] = 1.
         pylab.axis(a)
-    fout = os.path.join(dout, '%s.pdf' % froot)
-    pylab.savefig(fout)
+        pylab.subplot(2, 3, 3)
+        pylab.hist(y['curiosity'])
+        pylab.xlabel('curiosity')
+        a = list(pylab.axis())
+        a[0] = 0
+        a[1] = 1.
+        pylab.axis(a)
+        pylab.subplot(2, 3, 4)
+        pylab.plot(y['accuracy'], y['upper_bound'], '.', alpha=0.2)
+        pylab.xlabel('accuracy')
+        pylab.ylabel('upper_bound')
+        pylab.subplot(2, 3, 5)
+        pylab.plot(y['accuracy'], y['curiosity'], '.', alpha=0.2)
+        pylab.xlabel('accuracy')
+        pylab.ylabel('curiosity')
+        pylab.subplot(2, 3, 6)
+        pylab.plot(y['upper_bound'], y['curiosity'], '.', alpha=0.2)
+        pylab.xlabel('upper_bound')
+        pylab.ylabel('curiosity')        
+
+        fout = os.path.join(dout, '%s-length=%d.pdf' % (froot, i))
+        pylab.savefig(fout)
 
 din = os.path.join('..', 'cache')
 dout = os.path.join('..', 'figs')
@@ -47,5 +68,8 @@ make_figure(froot='serial_lazy-max_accuracy=0.999-max_length=5', din=din,
             dout=dout, max_accuracy=0.999, max_length=5)
 """
 
-make_figure(froot='tdata_R-serial_gc-max_accuracy=0.999-max_length=5',
-            din=din, dout=dout, max_accuracy=0.999, max_length=5)
+make_figure(froot='tdata_R-serial_gc-max_accuracy=0.999-max_length=6',
+            din=din, dout=dout, max_accuracy=0.999, max_length=6)
+
+#make_figure(froot='adult_R-serial_gc-max_accuracy=0.820-max_length=2',
+#            din=din, dout=dout, max_accuracy=0.820, max_length=2)
