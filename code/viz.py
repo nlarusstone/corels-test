57a47ac9783fcf968e9fd48a0f8b4dc6d9b86337
elaine84 <elaine.angelino@gmail.com>
2016-05-03 23:05:18 -0700

    Add visualizations of a growing prefix

--- code/viz.py
+++ code/viz.py
@@ -3,7 +3,18 @@ import numpy as np
 
 from branch_bound import print_rule_list
 
-def prefix_trace(prefix, cache, rule_names=None, ndata=None):
+
+def mpz_to_string(x):
+    # skip leading 1
+    return "{0}".format(x.digits(2))[1:]
+
+def mpz_to_array(x):
+    return np.cast['uint8']([i for i in mpz_to_string(x)])
+
+def array_to_string(x):
+    return ''.join(np.cast[str](x))
+
+def prefix_trace(prefix, cache, rule_names=None, ndata=None, rules=None, ones=None, m=100):
     n = len(prefix) + 1
     accuracy = np.zeros(n)
     upper_bound = np.zeros(n)
@@ -17,6 +28,72 @@ def prefix_trace(prefix, cache, rule_names=None, ndata=None):
         curiosity[i] = c.curiosity
         num_captured[i] = c.num_captured
         num_captured_correct[i] = c.num_captured_correct
+    print
+    print '0 = not captured'
+    print '1 = captured'
+    for i in range(1, n):
+        c = cache[prefix[:i]]
+        captured = 1 - mpz_to_array(c.not_captured)
+        print array_to_string(captured[:m])
+    if (rules is not None):
+        labels = mpz_to_array(ones)
+        print
+        print '0 = not captured'
+        print '1 = captured and not correct'
+        print '3 = captured and correct'
+        state = np.zeros(ndata, dtype='uint8')
+        for i in range(n):
+            pfx = prefix[:i]
+            c = cache[pfx]
+            captured = 1 - mpz_to_array(c.not_captured)
+            if i:
+                rule = mpz_to_array(rules[pfx[-1]])
+                correct = (labels == c.prediction[-1])
+                new_captured = (state == 0) & (captured == 1)
+                state[new_captured & np.invert(correct)] = 1
+                state[new_captured & correct] = 3
+            print array_to_string(state[:m])
+        print
+        print '0 = not captured and incorrect by default'
+        print '1 = captured and not correct'
+        print '2 = not captured and correct by default'
+        print '3 = captured and correct'
+        state = np.zeros(ndata, dtype='uint8')
+        captured_viz = np.zeros((n, m), int)
+        correct_viz = np.zeros((n, m), int)
+        complete_viz = np.zeros((n, m), int)
+        for i in range(n):
+            pfx = prefix[:i]
+            c = cache[pfx]
+            captured = 1 - mpz_to_array(c.not_captured)
+            captured_viz[i,:] = captured[:m].copy()
+            if i:
+                rule = mpz_to_array(rules[pfx[-1]])
+                correct = (labels == c.prediction[-1])
+                new_captured = (state == 0) & (captured == 1)
+                state[new_captured & np.invert(correct)] = 1
+                state[new_captured & correct] = 3
+                correct_viz[i,:] = state[:m].copy()
+            with_default = state.copy()
+            with_default[(state == 0) & (labels == c.default_rule)] = 2
+            complete_viz[i,:] = with_default[:m].copy()
+            print array_to_string(with_default[:m])
+        plt.figure(2, figsize=(14, 6))
+        plt.clf()
+        plt.pcolor(captured_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
+        plt.axis('tight')
+        plt.colorbar(ticks=range(-1, 4), format='%d')
+        plt.figure(3, figsize=(14, 6))
+        plt.clf()
+        plt.pcolor(correct_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
+        plt.axis('tight')
+        plt.colorbar(ticks=range(-1, 4), format='%d')
+        plt.figure(4, figsize=(14, 6))
+        plt.clf()
+        plt.pcolor(complete_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
+        plt.axis('tight')
+        plt.colorbar(ticks=range(-1, 4), format='%d')
+
     c = cache[prefix]
     print c
     if (rule_names is not None):
