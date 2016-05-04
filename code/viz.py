460d499ee97144a9b1ed39a603e084b332c9e7ef
Elaine Angelino <elaine.angelino@gmail.com>
2016-05-04 13:46:40 -0700

    Minor viz updates

--- code/viz.py
+++ code/viz.py
@@ -14,7 +14,8 @@ def mpz_to_array(x):
 def array_to_string(x):
     return ''.join(np.cast[str](x))
 
-def prefix_trace(prefix, cache, rule_names=None, ndata=None, rules=None, ones=None, m=98):
+def prefix_trace(prefix, cache, rule_names=None, ndata=None, rules=None,
+                 ones=None, m=150, quiet=True, fs=14):
     n = len(prefix) + 1
     accuracy = np.zeros(n)
     upper_bound = np.zeros(n)
@@ -28,36 +29,38 @@ def prefix_trace(prefix, cache, rule_names=None, ndata=None, rules=None, ones=No
         curiosity[i] = c.curiosity
         num_captured[i] = c.num_captured
         num_captured_correct[i] = c.num_captured_correct
-    print
-    print '0 = not captured'
-    print '1 = captured'
-    for i in range(1, n):
-        c = cache[prefix[:i]]
-        captured = 1 - mpz_to_array(c.not_captured)
-        print array_to_string(captured[:m])
-    if (rules is not None):
-        labels = mpz_to_array(ones)
+    if not quiet:
         print
         print '0 = not captured'
-        print '1 = captured and not correct'
-        print '3 = captured and correct'
-        state = np.zeros(ndata, dtype='uint8')
-        for i in range(n):
-            pfx = prefix[:i]
-            c = cache[pfx]
+        print '1 = captured'
+        for i in range(1, n):
+            c = cache[prefix[:i]]
             captured = 1 - mpz_to_array(c.not_captured)
-            if i:
-                rule = mpz_to_array(rules[pfx[-1]])
-                correct = (labels == c.prediction[-1])
-                new_captured = (state == 0) & (captured == 1)
-                state[new_captured & np.invert(correct)] = 1
-                state[new_captured & correct] = 3
-            print array_to_string(state[:m])
-        print
-        print '0 = not captured and incorrect by default'
-        print '1 = captured and not correct'
-        print '2 = not captured and correct by default'
-        print '3 = captured and correct'
+            print array_to_string(captured[:m])
+    if (rules is not None):
+        labels = mpz_to_array(ones)
+        if not quiet:
+            print
+            print '0 = not captured'
+            print '1 = captured and not correct'
+            print '3 = captured and correct'
+            state = np.zeros(ndata, dtype='uint8')
+            for i in range(n):
+                pfx = prefix[:i]
+                c = cache[pfx]
+                captured = 1 - mpz_to_array(c.not_captured)
+                if i:
+                    rule = mpz_to_array(rules[pfx[-1]])
+                    correct = (labels == c.prediction[-1])
+                    new_captured = (state == 0) & (captured == 1)
+                    state[new_captured & np.invert(correct)] = 1
+                    state[new_captured & correct] = 3
+                print array_to_string(state[:m])
+            print
+            print '0 = not captured and incorrect by default'
+            print '1 = captured and not correct'
+            print '2 = not captured and correct by default'
+            print '3 = captured and correct'
         state = np.zeros(ndata, dtype='uint8')
         captured_viz = np.zeros((n, m), int)
         correct_viz = np.zeros((n, m), int)
@@ -77,41 +80,54 @@ def prefix_trace(prefix, cache, rule_names=None, ndata=None, rules=None, ones=No
             with_default = state.copy()
             with_default[(state == 0) & (labels == c.default_rule)] = 2
             complete_viz[i,:] = with_default[:m].copy()
-            print array_to_string(with_default[:m])
+            if not quiet:
+                print array_to_string(with_default[:m])
         plt.figure(2, figsize=(14, 6))
         plt.clf()
         plt.pcolor(captured_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
-        plt.axis('tight')
-        plt.colorbar(ticks=range(-1, 4), format='%d')
+        plt.title('(dark blue) uncaptured, (light blue) captured', fontsize=fs)
+        plt.xlabel('data index', fontsize=fs)
         plt.figure(3, figsize=(14, 6))
         plt.clf()
         plt.pcolor(correct_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
-        plt.axis('tight')
-        plt.colorbar(ticks=range(-1, 4), format='%d')
+        plt.title('(dark blue) uncaptured, (red) captured & correct\n' +
+                  '(light blue) captured & incorrect', fontsize=fs)
         plt.figure(4, figsize=(14, 6))
         plt.clf()
         plt.pcolor(complete_viz[::-1], cmap='coolwarm', vmin=0, vmax=3)
-        plt.axis('tight')
-        plt.colorbar(ticks=range(-1, 4), format='%d')
+        plt.title('(pink) uncaptured & correct by default, ' +
+                  '(red) captured & correct\n' +
+                  '(dark blue) uncaptured & incorrect by default, ' +
+                  '(light blue) captured & incorrect', fontsize=fs)
+        for fig in [2, 3, 4]:
+            plt.figure(fig)
+            plt.xlabel('data index', fontsize=fs)
+            plt.ylabel('prefix length', fontsize=fs)
+            plt.xticks(fontsize=fs)
+            plt.yticks(np.arange(n) + 0.5, range(n)[::-1], fontsize=fs)
+            plt.axis('tight')
+            # plt.colorbar(ticks=range(-1, 4), format='%d')
 
     c = cache[prefix]
     print c
     if (rule_names is not None):
         print_rule_list(prefix, c.prediction, c.default_rule, rule_names)    
-    plt.figure(1, figsize=(10, 4))
+    plt.figure(1, figsize=(12, 4))
     plt.clf()
     plt.subplot(1, 2, 1)
     plt.plot(range(n), upper_bound, ':', marker='.')
     plt.plot(range(n), accuracy, '-', marker='.')
     plt.plot(range(n), curiosity, '--', marker='.')
-    plt.axis('tight')
     plt.legend(('upper bound', 'accuracy', 'curiosity'), loc='best')
     plt.subplot(1, 2, 2)
-    plt.plot(range(n), num_captured, ':', marker='.')
-    plt.plot(range(n), num_captured_correct, '-', marker='.')
+    plt.plot(range(n), num_captured / float(ndata), ':', marker='.')
+    plt.plot(range(n), num_captured_correct / float(ndata), '-', marker='.')
     a = list(plt.axis('tight'))
-    plt.legend(('num captured', 'num captured correct'), loc='best')
-    if (ndata is not None):
-        a[3] = ndata
-        plt.axis(a)
+    plt.legend(('fraction captured', 'fraction captured & correct'), loc='best')
+    for j in [1, 2]:
+        plt.subplot(1, 2, j)
+        plt.axis([0, n - 1, 0, 1])
+        plt.xlabel('prefix length', fontsize=fs)
+        plt.xticks(fontsize=fs)
+        plt.yticks(fontsize=fs)
     return
