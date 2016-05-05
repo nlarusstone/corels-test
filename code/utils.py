61ac1cc2ad90d62d243c6929fd7b740c63a469f3
Elaine Angelino <elaine.angelino@gmail.com>
2016-05-05 15:51:20 -0700

    Find rules that commute

--- code/utils.py
+++ code/utils.py
@@ -13,3 +13,13 @@ def array_to_string(x):
 
 def rules_to_array(x):
     return np.array([mpz_to_array(r) for r in x])
+
+def relationships(x):
+    n = x.shape[0]
+    num_pairs = (n * (n - 1)) / 2
+    d = np.dot(x, x.T)
+    num_commuting_pairs = (d == 0).sum() / 2
+    print num_pairs, num_commuting_pairs
+    mask = np.triu(np.ones(d.shape, int))
+    mask = mask - np.identity(d.shape[0], int)
+    return zip(((d == 0) & mask).nonzero())
