d4b4663c317b96919990b313cc5b50b7ae04b6b5
elaine angelino <elaine.angelino@gmail.com>
2016-05-15 17:19:06 -0700

    Improve figures

--- code/figs.py
+++ code/figs.py
@@ -8,52 +8,77 @@ def viz_log(metadata, din, dout, delimiter=','):
     pass
 
 def make_figure(metadata, din, dout, max_accuracy, max_length, delimiter='\t',
-                alpha=0.05):
+                alpha=0.05, lw=3, fs=14):
     fin = os.path.join(din, '%s.txt' % metadata)
     x = tb.tabarray(SVfile=fin, delimiter=delimiter)
     pylab.ion()
+    pylab.figure(1, figsize=(16, 10))
+    pylab.clf()
     for i in range(1, max_length + 1):
         print i
-        pylab.figure(1, figsize=(14, 8))
-        pylab.clf()
         y = x[x['length'] == i]
-        pylab.subplot(2, 3, 1)
+
+        pylab.subplot(max_length, 3, (i - 1) * 3 + 1)
+        pylab.hist(y['objective'])
+        pylab.hist(y['lower_bound'])
+        # pylab.legend(('objective', 'lower bound'), loc='upper left')
+        a = list(pylab.axis())
+        min_objective = y['objective'].min()
+        pylab.plot([min_objective] * 2, [0, a[3]], 'b:', linewidth=lw)
+        max_lower_bound = y['lower_bound'].max()
+        pylab.plot([max_lower_bound] * 2, [0, a[3]], 'g:', linewidth=lw)
+        pylab.ylabel('%d' % len(y))
+        pylab.yticks([pylab.yticks()[0][-2]])
+        if (i == 1):
+            pylab.title('lower bound & objective', fontsize=fs)
+            a1 = pylab.axis()
+        else:
+            a = list(pylab.axis())
+            a[0] = a1[0]
+            a[1] = a1[1]
+            pylab.axis(a)
+        if (i < max_length):
+            pylab.xticks([], [])
+
+        pylab.subplot(max_length, 3, (i - 1) * 3 + 2)
         pylab.hist(y['accuracy'])
-        pylab.xlabel('accuracy')
+        pylab.hist(y['upper_bound'])
+        # pylab.legend(('accuracy', 'upper bound'), loc='upper right')
         a = list(pylab.axis())
         a[0] = 0.5
         a[1] = 1.
         max_accuracy = y['accuracy'].max()
-        pylab.plot([max_accuracy, max_accuracy], [0, a[3]], 'r-')
-        pylab.axis(a)
-        pylab.subplot(2, 3, 2)
-        pylab.hist(y['upper_bound'])
-        pylab.xlabel('upper_bound')
-        a = list(pylab.axis())
-        a[0] = max_accuracy
-        a[1] = 1.
-        pylab.axis(a)
-        pylab.subplot(2, 3, 3)
+        pylab.plot([max_accuracy] * 2, [0, a[3]], 'b:', linewidth=lw)
+        min_upper_bound = y['upper_bound'].min()
+        pylab.plot([min_upper_bound] * 2, [0, a[3]], 'g:', linewidth=lw)
+        pylab.yticks([pylab.yticks()[0][-2]])
+        if (i == 1):
+            pylab.title('accuracy & upper bound', fontsize=fs)
+            a2 = pylab.axis()
+        else:
+            a = list(pylab.axis())
+            a[0] = a2[0]
+            a[1] = a2[1]
+            pylab.axis(a)
+        if (i < max_length):
+            pylab.xticks([], [])
+
+        pylab.subplot(max_length, 3, (i - 1) * 3 + 3)
         pylab.hist(y['curiosity'])
-        pylab.xlabel('curiosity')
-        a = list(pylab.axis())
-        a[0] = 0
-        a[1] = 1.
-        pylab.axis(a)
-        pylab.subplot(2, 3, 4)
-        pylab.plot(y['accuracy'], y['upper_bound'], '.', alpha=alpha)
-        pylab.xlabel('accuracy')
-        pylab.ylabel('upper_bound')
-        pylab.subplot(2, 3, 5)
-        pylab.plot(y['accuracy'], y['curiosity'], '.', alpha=alpha)
-        pylab.xlabel('accuracy')
-        pylab.ylabel('curiosity')
-        pylab.subplot(2, 3, 6)
-        pylab.plot(y['upper_bound'], y['curiosity'], '.', alpha=alpha)
-        pylab.xlabel('upper_bound')
-        pylab.ylabel('curiosity')        
+        pylab.yticks([pylab.yticks()[0][-2]])
+        if (i == 1):
+            pylab.title('curiosity', fontsize=fs)
+            a3 = pylab.axis()
+        else:
+            a = list(pylab.axis())
+            a[0] = a3[0]
+            a[1] = a3[1]
+            pylab.axis(a)
+        if (i < max_length):
+            pylab.xticks([], [])
 
         fout = os.path.join(dout, '%s-length=%d.png' % (metadata, i))
+        pylab.suptitle(metadata.replace('-', ', '), fontsize=fs)
         pylab.savefig(fout)
 
 din = os.path.join('..', 'cache')
