535cb5531fa10985f69573f8cf7730d791d9195c
elaine angelino <elaine.angelino@gmail.com>
2017-01-05 20:59:44 -0800

    Stats on minority support for each equivalence class

--- code/minority.py
+++ code/minority.py
@@ -8,7 +8,7 @@ y = tb.tabarray(SVfile='../data/adult_R.label')
 y = y[list(y.dtype.names[1:])].extract()
 
 z = x[list(x.dtype.names)[1:]].extract().T
-s = [str(tuple(i)) for i in z]
+s = np.array([str(tuple(i)) for i in z])
 
 r = tb.tabarray(columns=[s, y, np.ones(len(s), int)], names=['obs', 'label', 'count'])
 
@@ -33,5 +33,21 @@ ind = np.array([int(d[obs] == label) for (obs, label) in zip(r['obs'], y)])
 assert (ind.sum() == m.sum())
 
 f = open('../data/adult_R.minor', 'w')
-f.write('{group_minority} ' + ' '.join([str(i) for i in ind]))
+f.write('{group_minority} ' + ' '.join([str(i) for i in ind]) + '\n')
 f.close()
+
+ms = m.copy()
+ms.sort()
+ms = ms[::-1]
+
+print 'mean (min, max) minority support within an equivalence class: %1.3f (%d, %d)' % (ms.mean(), ms.min(), ms.max())
+print 'number of equivalence classes with minority support > 10 : %d' % (ms > 10).sum()
+print 'number of equivalence classes with minority support > 1 : %d' % (ms > 1).sum()
+print 'number of equivalence classes with minority support > 0 : %d' % (ms > 0).sum()
+
+"""
+mean (min, max) minority support within an equivalence class: 0.318 (0, 75)
+number of equivalence classes with minority support > 10 : 55
+number of equivalence classes with minority support > 1 : 450
+number of equivalence classes with minority support > 0 : 1120
+"""
