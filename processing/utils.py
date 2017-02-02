22e46b721fbcf4d409b4bf47118bd24ebce85d18
elaine angelino <elaine.angelino@gmail.com>
2017-02-01 17:17:40 -0800

    Don't generate redundant binary features
    For compas it seems reasonable to have 1 feature for binary
    features and n features for categorical features with n classes

--- processing/utils.py
+++ processing/utils.py
@@ -7,6 +7,9 @@ def to_binary(x):
     names = []
     for n in x.dtype.names:
         dlist = list(set(x[n]))
+        dlist.sort()
+        if (len(dlist) == 2):
+            dlist = dlist[1:]
         for d in dlist:
             names.append('%s:%s' % (n, str(d)))
             columns.append(np.cast[int](x[n] == d))
