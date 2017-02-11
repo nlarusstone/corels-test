5e99ddf96da4bee3898d5c151c799fe4419ccc3b
Elaine Angelino <elaine.angelino@gmail.com>
2017-02-11 00:45:03 -0800

    Minor edits to parser for Compare.sh output
    Because RIPPER crashes for frisk

--- eval/compare.py
+++ eval/compare.py
@@ -1,50 +1,57 @@
 import numpy as np
 import pylab
 
+figure = False
 
-names = ['GLM', 'SVM', 'AdaBoost\n\n', 'CART', 'C4.5', 'RF', 'RIPPER\n', 'SBRL', 'BBRL']
 
+# COMPAS
+names = ['GLM', 'SVM', 'AdaBoost\n\n', 'CART', 'C4.5', 'RF', 'RIPPER\n', 'SBRL', 'CORELS']
 x = open('../compare/compas.txt', 'rU').read().strip().split('\n')
 
-sbrl = [line.strip().split()[-1] for line in x if line.startswith('test accuracy')]
+# weapon
+names = ['GLM', 'SVM', 'AdaBoost\n\n', 'CART', 'C4.5', 'RF', 'SBRL', 'CORELS']
+x = open('../compare/weapon.txt', 'rU').read().strip().split('\n')
+
+sbrl = [line.strip().split()[3] for line in x if line.startswith('test accuracy')]
 sbrl = np.array(sbrl)
 
 other = [line.strip().split() for line in x if line.startswith('0')]
 other = np.array(other)
 
-bbrl = [line for line in x if line.startswith('Test accuracies')][0]
-bbrl = np.array(bbrl.strip().split('[')[1].split(']')[0].split(','))
+corels = [line for line in x if line.startswith('Test accuracies')][0]
+corels = np.array(corels.strip().split('[')[1].split(']')[0].split(','))
 
-y = np.cast[float](np.vstack([other.T, sbrl, bbrl]).T)
+y = np.cast[float](np.vstack([other.T, sbrl, corels]).T)
 
-print '\n'.join(['%2.1f $\\pm$ %2.1f' % (m, s) for (m, s) in zip(y.mean(axis=0) * 100, y.std(axis=0) * 100)])
+print '\n'.join(['%s & %2.1f $\\pm$ %2.1f' % (n.strip(), m, s) for (n, m, s) in zip(names, y.mean(axis=0) * 100, y.std(axis=0) * 100)])
 
-pylab.ion()
-pylab.figure(1, figsize=(7, 5))
-pylab.clf()
-pylab.subplot2grid((10, 20), (0, 1), colspan=19, rowspan=9)
+if figure:
+    pylab.ion()
+    pylab.figure(1, figsize=(7, 5))
+    pylab.clf()
+    pylab.subplot2grid((10, 20), (0, 1), colspan=19, rowspan=9)
 
-fs=16
+    fs=16
 
-(nfolds, nmethods) = y.shape
+    (nfolds, nmethods) = y.shape
 
-color_vec = ['r', 'orange', 'y', 'g', 'c', 'b', 'purple', 'violet', 'm', 'gray']
-ii = y[:,-1].argsort()[::-1]
+    color_vec = ['r', 'orange', 'y', 'g', 'c', 'b', 'purple', 'violet', 'm', 'gray']
+    ii = y[:,-1].argsort()[::-1]
 
-pylab.errorbar(range(nmethods), y.mean(axis=0), y.std(axis=0), fmt=None, ecolor='k', elinewidth=2, capsize=10, capthick=2)
+    pylab.errorbar(range(nmethods), y.mean(axis=0), y.std(axis=0), fmt=None, ecolor='k', elinewidth=2, capsize=10, capthick=2)
 
-for (i, color) in zip(ii, color_vec):
-    pylab.plot(range(nmethods), y[i, :], 'D', color=color, markeredgewidth=0, markersize=8)
+    for (i, color) in zip(ii, color_vec):
+        pylab.plot(range(nmethods), y[i, :], 'D', color=color, markeredgewidth=0, markersize=8)
 
-pylab.plot(range(nmethods), y.mean(axis=0), 's', color='white', markeredgewidth=2, markersize=8)
+    pylab.plot(range(nmethods), y.mean(axis=0), 's', color='white', markeredgewidth=2, markersize=8)
 
-pylab.xticks(range(nmethods), names, fontsize=fs, rotation=40)
-pylab.yticks(fontsize=fs)
-pylab.ylabel('accuracy\n', fontsize=fs)
+    pylab.xticks(range(nmethods), names, fontsize=fs, rotation=40)
+    pylab.yticks(fontsize=fs)
+    pylab.ylabel('accuracy\n', fontsize=fs)
 
-a = list(pylab.axis())
-a[0] -= 1
-a[1] += 1
-pylab.axis(a)
+    a = list(pylab.axis())
+    a[0] -= 1
+    a[1] += 1
+    pylab.axis(a)
 
-#pylab.savefig('../paper/figs/compare-compas.pdf')
+    pylab.savefig('../paper/figs/compare-compas.pdf')
