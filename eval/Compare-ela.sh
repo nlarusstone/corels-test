9a334c85307e8840accc8f4ca69e5bbcade3abb7
Daniel Alabi <alabidan@gmail.com>
2017-02-14 04:09:35 -0500

    set seed for sbrl

--- eval/Compare-ela.sh
+++ eval/Compare-ela.sh
@@ -20,7 +20,7 @@ do
     cv_fold=${dataset}_${i}
     cv_fold_path=${cv_dir}/${cv_fold}
     echo "\nCV fold: ${cv_fold}\n"
-    sbrl_run=$(printf '%s -t 3 -d 1 %s_train.out %s_train.label %s_test.out %s_test.label' "$sbrl" "$cv_fold_path" "$cv_fold_path" "$cv_fold_path" "$cv_fold_path")
+    sbrl_run=$(printf '%s -t 3 -d 1 -S 0 %s_train.out %s_train.label %s_test.out %s_test.label' "$sbrl" "$cv_fold_path" "$cv_fold_path" "$cv_fold_path" "$cv_fold_path")
     echo "RUNNING SBRL"
     eval "$sbrl_run" >> $temp_f 2>&1
     echo $(tail -n 2 $temp_f)
