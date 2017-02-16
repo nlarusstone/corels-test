9d9b9b1fcfce2533e22295a233b5fa25595f45f5
Elaine Angelino <elaine.angelino@gmail.com>
2017-02-15 16:56:18 -0800

    Report training error in CompareSparsity.R

--- eval/CompareSparsity.R
+++ eval/CompareSparsity.R
@@ -57,6 +57,7 @@ testDataWOClass <- subset(testData, select=-c(Class))
 ## CART
 ## complexity (cp) parameters -> 0.01 (default), 0.003, 0.001, 0.03, 0.1
 cartAccs <- c()
+cartTrainAccs <- c()
 cartLeaves <- c()
 cps <- c(0.001, 0.003, 0.01, 0.03, 0.1)
 cartResults <- data.frame(stringsAsFactors=F)
@@ -64,22 +65,29 @@ cartResults <- data.frame(stringsAsFactors=F)
 for (val in cps) {
     cartModel <- rpart(Class ~ . , data=as.data.frame(trainData),
                        control = rpart.control(cp=val))
+
     pred.cartModel <- round(predict(cartModel, newdata=as.data.frame(testDataWOClass)))
     acc <- sum(testData$Class == factor(pred.cartModel[,"X1"], labels=sortednames))/length(testData$Class)
     cartAccs <- c(cartAccs, acc)
+
+    trainPred.cartModel <- round(predict(cartModel, newdata=as.data.frame(trainDataWOClass)))
+    tacc <- sum(trainData$Class == factor(trainPred.cartModel[,"X1"], labels=sortednames))/length(trainData$Class)
+    cartTrainAccs <- c(cartTrainAccs, tacc)
+
     leaves <- length(which(cartModel$frame[,"var"]=="<leaf>"))
     cartLeaves <- c(cartLeaves, leaves)
-    cartResults <- rbind(cartResults, list(fname, "CART", 0.0, val, 0.0, acc, leaves))
+    cartResults <- rbind(cartResults, list(fname, "CART", 0.0, val, 0.0, acc, leaves, tacc))
 }
 printf("CART:\n")
 printf("%s", cat(cps, "\n"))
 printf("%s", cat(cartAccs, "\n"))
 printf("%s", cat(cartLeaves, "\n"))
-
+printf("%s", cat(cartTrainAccs, "\n"))
 
 ## C4.5
 ## complexity (C) parameters -> 0.05, 0.15, 0.25 (default), 0.35, 0.45
 c45Accs <- c()
+c45TrainAccs <- c()
 c45Leaves <- c()
 Cs <- c(0.05, 0.15, 0.25, 0.35, 0.45)
 c45Results <- data.frame(stringsAsFactors=F)
@@ -89,34 +97,48 @@ for (val in Cs) {
     data_train_fac[,"Class"] <- as.factor(data_train_fac[,"Class"])
     c45Model <- J48(Class ~ ., data=as.data.frame(data_train_fac),
                     control = Weka_control(C=val))
+
     pred.c45Model <- predict(c45Model, newdata=as.data.frame(testDataWOClass), type="class")
     acc <- sum(testData$Class == pred.c45Model)/length(testData$Class)
     c45Accs <- c(c45Accs, acc)
+
+    trainPred.c45Model <- predict(c45Model, newdata=as.data.frame(trainDataWOClass), type="class")
+    tacc <- sum(trainData$Class == trainPred.c45Model)/length(trainData$Class)
+    c45TrainAccs <- c(c45TrainAccs, tacc)
+
     leaves <- c45Model$classifier$measureNumLeaves()
     c45Leaves <- c(c45Leaves, leaves)
-    c45Results <- rbind(c45Results, list(fname, "C4.5", val, 0.0, 0.0, acc, leaves))
+    c45Results <- rbind(c45Results, list(fname, "C4.5", val, 0.0, 0.0, acc, leaves, tacc))
 }
 printf("C4.5:\n")
 printf("%s", cat(Cs, "\n"))
 printf("%s", cat(c45Accs, "\n"))
 printf("%s", cat(c45Leaves, "\n"))
-
+printf("%s", cat(c45TrainAccs, "\n"))
 
 ## RIPPER
 ripResults <- data.frame(stringsAsFactors=F)
 ripModel <- JRip(Class ~ . , data=as.data.frame(trainData))
+
 pred.ripModel <- predict(ripModel, newdata=as.data.frame(testDataWOClass), type="class")
 ripAcc <- sum(testData$Class == pred.ripModel)/length(testData$Class)
+
+trainPred.ripModel <- predict(ripModel, newdata=as.data.frame(trainDataWOClass), type="class")
+ripTrainAcc <- sum(trainData$Class == trainPred.ripModel)/length(trainData$Class)
+
 ripRuleLen <- length(as.list(ripModel$classifier$getRuleset()))
-ripResults <- rbind(ripResults, list(fname, "RIPPER", 0.0, 0.0, 0.0, ripAcc, ripRuleLen))
+ripResults <- rbind(ripResults, list(fname, "RIPPER", 0.0, 0.0, 0.0, ripAcc, ripRuleLen, ripTrainAcc))
+
+
 printf("RIPPER:\n")
 printf("%.4f\n", ripAcc)
 printf("%d\n", ripRuleLen)
+printf("%.4f\n", ripTrainAcc)
 
 ## Write out results
-colnames(cartResults) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves")
-colnames(c45Results) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves")
-colnames(ripResults) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves")
+colnames(cartResults) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves", "train_accuracy")
+colnames(c45Results) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves", "train_accuracy")
+colnames(ripResults) <- c("Fold", "Method", "C", "cp", "R", "accuracy", "leaves", "train_accuracy")
 isNewFile <- is.na(file.info(foutput)$size) || file.info(foutput)$size == 0
 write.table(cartResults, foutput, row.names=F, col.names=isNewFile,
             append=!isNewFile, quote = F, sep=",")
