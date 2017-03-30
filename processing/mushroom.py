c3a3ce0660844d4118f4be8aed2e78d40439f4f4
elaine84 <elaine.angelino@gmail.com>
2017-03-29 21:30:23 -0700

    Was missing gill-size for mushroom

--- processing/mushroom.py
+++ processing/mushroom.py
@@ -43,9 +43,8 @@ if not os.path.exists(fdata):
     os.system('wget %sagaricus-lepiota.data -O %s' % (uroot, fdata))
     os.system('wget %sagaricus-lepiota.names -O %s' % (uroot, fnames))
 
-names = ['poisonous-or-edible',
-         'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor',
-         'gill-attachment', 'gill-spacing', 'gill-color', 'stalk-shape',
+names = ['poisonous', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor',
+         'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape',
          'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring',
          'stalk-color-above-ring', 'stalk-color-below-ring',
          'veil-type', 'veil-color', 'ring-number', 'ring-type',
