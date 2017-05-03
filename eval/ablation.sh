354678e3fe6f0666155095f09a7322d7e80fa3a3
elaine angelino <elaine.angelino@gmail.com>
2017-05-02 23:51:15 -0700

    Add arg for regularization param in ablation script

--- eval/ablation.sh
+++ eval/ablation.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# ./ablation.sh compas 1000000000 none
-# ./ablation.sh compas 1000000000 identical
+# ./ablation.sh compas 1000000000 none 0.005
+# ./ablation.sh compas 1000000000 identical 0.005
+# ./ablation.sh weapon 1000000000 none 0.01
 # etc.
 
 args=("$@")
 dataset=${args[0]}
 n=${args[1]}
 ablation=${args[2]}
-
+r=${args[3]}
 
 for i in `seq 0 9`;
 do
@@ -18,21 +19,21 @@ do
     minor=../data/CrossValidation/${dataset}_${i}_train.minor
     if [ "$ablation" = "none" ];
     then
-        $bbcache -n ${n} -c 2 -r 0.005 -p 1 $out $label $minor &
+        $bbcache -n ${n} -c 2 -r $r -p 1 $out $label $minor &
     elif [ "$ablation" = "priority" ];
     then
-        $bbcache -n ${n} -b -r 0.005 -p 1 $out $label $minor &
+        $bbcache -n ${n} -b -r $r -p 1 $out $label $minor &
     elif [ "$ablation" = "support" ];
     then
-        $bbcache -n ${n} -c 2 -r 0.005 -p 1 -a 1 $out $label $minor &
+        $bbcache -n ${n} -c 2 -r $r -p 1 -a 1 $out $label $minor &
     elif [ "$ablation" = "pmap" ];
     then
-        $bbcache -n ${n} -c 2 -r 0.005 $out $label $minor &
+        $bbcache -n ${n} -c 2 -r $r $out $label $minor &
     elif [ "$ablation" = "lookahead" ];
     then
-        $bbcache -n ${n} -c 2 -r 0.005 -p 1 -a 2 $out $label $minor &
+        $bbcache -n ${n} -c 2 -r $r -p 1 -a 2 $out $label $minor &
     elif [ "$ablation" = "identical" ];
     then
-        $bbcache -n ${n} -c 2 -r 0.005 -p 1 $out $label
+        $bbcache -n ${n} -c 2 -r $r -p 1 $out $label
     fi
 done
