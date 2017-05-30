9798acab67e64b67a113fd6f1b1fbdf68de7386a
elaine angelino <elaine.angelino@gmail.com>
2017-05-29 22:31:41 -0700

    Add small figure for different reg params

--- viz/weapon_reg_execution.py
+++ viz/weapon_reg_execution.py
@@ -22,7 +22,7 @@ fs = 16 # fontsize
 num_folds = 10
 make_figure = True
 figure_fold = -1
-make_small = False
+make_small = True
 
 num_folds = 1
 figure_fold = 0
@@ -45,7 +45,13 @@ fs_legend = fs - 2
 
 if (make_figure):
     pylab.ion()
-    pylab.figure(6, figsize=(16, 5.8))
+    if (make_small):
+        pylab.figure(5, figsize=(12, 6))
+        ftag = '%s_small' % ftag
+        fs += 3
+        fs_legend += 3
+    else:
+        pylab.figure(6, figsize=(16, 5.8))
     pylab.clf()
 
 ntot = len(log_root_list)
@@ -85,7 +91,11 @@ for (ncomp, log_root) in enumerate(log_root_list):
     imin = np.nonzero(x['tree_min_objective'] == opt)[0][0]
     tmin = x['total_time'][imin]
 
-    pylab.subplot(2, 4, ncomp + 1)
+    if (make_small):
+        pylab.subplot(2, 2, ncomp + 1)
+    else:
+        pylab.subplot(2, 4, ncomp + 1)
+
     pylab.semilogx(x['total_time'][2:ii], x['tree_min_objective'][2:ii], '-', color='gray', linewidth=lw)
     pylab.semilogx(x['total_time'][2:ii], x['current_lower_bound'][2:ii], '-', color='coral', linewidth=lw*2)
     pylab.semilogx(tmin, opt, 'k*', markersize=18)
@@ -98,7 +108,10 @@ for (ncomp, log_root) in enumerate(log_root_list):
         pylab.text(tt * 1.1, oo + 0.025, str(pl), fontsize=fs-2)
 
     pylab.xticks(fontsize=fs-1)
-    pylab.title(labels[ncomp], fontsize=fs)
+    if (make_small):
+        pylab.text(10**-1.2, 0.46, labels[ncomp], fontsize=fs)
+    else:
+        pylab.title(labels[ncomp], fontsize=fs)
     pylab.xticks(fontsize=fs-2)
     pylab.yticks(np.arange(0, 0.59, 0.1), fontsize=fs)
     pylab.axis([x['total_time'][2], 10**3, 0, 0.54])
@@ -106,36 +119,49 @@ for (ncomp, log_root) in enumerate(log_root_list):
         pylab.ylabel('Value', fontsize=fs)
         pylab.legend(['Objective', 'Lower bound', 'Optimum'], loc='lower right', fontsize=fs_legend, numpoints=1, frameon=False)
 
-    pylab.subplot(2, 4, 4)
+    if (make_small):
+        if (ncomp == 2):
+            pylab.ylabel('Value', fontsize=fs)
+            pylab.xticks(fontsize=fs-1)
+            pylab.xlabel('Time (s)', fontsize=fs)
+        pylab.subplot(2, 2, 4)
+    else:
+        pylab.subplot(2, 4, 4)
+
     pylab.semilogx(x['total_time'][2:ii], x['tree_min_objective'][2:ii], '-', color=c1[ncomp], linewidth=lw)
     pylab.semilogx(x['total_time'][2:ii], x['current_lower_bound'][2:ii], '-', color=c1[ncomp], linewidth=lw*2)
     #pylab.semilogx(tmin, opt, 'k*', markersize=18)
     pylab.xticks(fontsize=fs-1)
     pylab.yticks(np.arange(0, 0.59, 0.1), fontsize=fs)
     pylab.axis([x['total_time'][2], 10**3, 0, 0.54])
-    pylab.title('Overlay', fontsize=fs)
-
-    pylab.subplot(2, 4, ncomp + 5)
-    xremaining = x['log_remaining_space_size'].copy()
-    xremaining[xremaining > xremaining[0]] = xremaining[0]
-    xtt = np.concatenate((x['total_time'][2:ii+1], x['total_time'][ii:ii+1]))
-    xrr = np.concatenate((xremaining[2:ii+1], [1.]))
-    pylab.semilogx(xtt, xrr, '-', color='c', linewidth=lw*2)
-
-    if (ncomp % 3 == 0):
-        pylab.ylabel('log10(Size)', fontsize=fs)
-        pylab.legend(['Upper bound on\nsize of remaining\nsearch space'], fontsize=fs_legend, loc='best', frameon=False)
-    pylab.xlabel('Time (s)', fontsize=fs)
+    if (make_small):
+        pylab.text(10**-1.5, 0.45, ' Overlay', fontsize=fs)
+    else:
+        pylab.title('Overlay', fontsize=fs)
     pylab.xticks(fontsize=fs-1)
-    pylab.yticks(range(0, 40, 10), fontsize=fs)
-    pylab.axis([x['total_time'][2], 10**3, 0, 32])
 
-    pylab.subplot(2, 4, 8)
-    pylab.semilogx(xtt, xrr, '-', color=c1[ncomp], linewidth=lw*2)
-    pylab.xlabel('Time (s)', fontsize=fs)
-    pylab.xticks(fontsize=fs-1)
-    pylab.yticks(range(0, 40, 10), fontsize=fs)
-    pylab.axis([x['total_time'][2], 10**3, 0, 32])
+    if (make_small):
+        pylab.xlabel('Time (s)', fontsize=fs)
+    else:
+        pylab.subplot(2, 4, ncomp + 5)
+        xremaining = x['log_remaining_space_size'].copy()
+        xremaining[xremaining > xremaining[0]] = xremaining[0]
+        xtt = np.concatenate((x['total_time'][2:ii+1], x['total_time'][ii:ii+1]))
+        xrr = np.concatenate((xremaining[2:ii+1], [1.]))
+        pylab.semilogx(xtt, xrr, '-', color='c', linewidth=lw*2)
+        if (ncomp % 3 == 0):
+            pylab.ylabel('log10(Size)', fontsize=fs)
+            pylab.legend(['Upper bound on\nsize of remaining\nsearch space'], fontsize=fs_legend, loc='best', frameon=False)
+        pylab.xlabel('Time (s)', fontsize=fs)
+        pylab.xticks(fontsize=fs-1)
+        pylab.yticks(range(0, 40, 10), fontsize=fs)
+        pylab.axis([x['total_time'][2], 10**3, 0, 32])
+        pylab.subplot(2, 4, 8)
+        pylab.semilogx(xtt, xrr, '-', color=c1[ncomp], linewidth=lw*2)
+        pylab.yticks(range(0, 40, 10), fontsize=fs)
+        pylab.axis([x['total_time'][2], 10**3, 0, 32])
+        pylab.xlabel('Time (s)', fontsize=fs)
+        pylab.xticks(fontsize=fs-1)
 
 #pylab.suptitle('Execution progress for different regularization parameters (NYCLU dataset)\n', fontsize=fs+2)
 pylab.savefig('../figs/%s-execution.pdf' % ftag)
