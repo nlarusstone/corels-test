dbc866ff58daee5503a7fb2778e0cedf7c8ad260
Elaine Angelino <elaine.angelino@gmail.com>
2017-06-08 17:34:08 -0700

    Add frisk sparsity figure with broken axis for C4.5

--- viz/kdd_frisk_sparsity_c45.py
+++ viz/kdd_frisk_sparsity_c45.py
@@ -13,6 +13,9 @@ log2 = False
 plt.ion()
 
 z = tb.tabarray(SVfile='../eval/frisk_sparsity.csv')
+z = z[z['Method'] != 'C4.5']
+q = tb.tabarray(SVfile='../eval/frisk_sparsity-c45.csv')
+z = z.rowstack(q)
 b = tb.tabarray(SVfile='../eval/frisk_sparsity-sbrl.csv', names=z.dtype.names, namesinheader=False)
 y = tb.tabarray(SVfile='../eval/frisk_sparsity-CORELS.csv', names=z.dtype.names, namesinheader=False)
 
@@ -25,27 +28,28 @@ s = x.aggregate(On=['Method', 'C', 'cp', 'R'], AggFuncDict={'accuracy': np.std, 
 #cm = c45.aggregate(On=['Method', 'C', 'cp', 'R'], AggFuncDict={'accuracy': np.mean, 'leaves': np.mean, 'train_accuracy': np.mean})
 #cs = c45.aggregate(On=['Method', 'C', 'cp', 'R'], AggFuncDict={'accuracy': np.std, 'leaves': np.std, 'train_accuracy': np.std})
 
-fig = plt.figure(2, figsize=(8, 3.6))
+fig = plt.figure(2, figsize=(8, 3.5))
 plt.clf()
 #ax = plt.subplot2grid((20, 1), (0, 1), colspan=1, rowspan=18)
 
 m.sort(order=['Method', 'C', 'cp', 'R'])
 s.sort(order=['Method', 'C', 'cp', 'R'])
 
-ind = [-4, -3, -2] + [-1] + [5, 6, 7, 9] + [0, 1, 4]
+ind = [-4, -3, -2] + [-1] + [4, 5, 6, 8] + [0, 1, 3]
 m = m[ind].copy()
 s = s[ind].copy()
 
 data = zip(m['Method'], m['leaves'], m['accuracy'], s['leaves'], s['accuracy'], m['train_accuracy'], s['train_accuracy'])
 
 ms = 5
-cdict = {'CORELS': 'r', 'C4.5': 'c', 'CART': 'gray', 'RIPPER': 'k', 'SBRL': 'purple'}
-mdict = {'CORELS': 's', 'C4.5': '^', 'CART': 'd', 'RIPPER': 'v', 'SBRL': 'o'}
+cdict = {'CORELS': 'k', 'C4.5': 'k', 'CART': 'k', 'RIPPER': 'k', 'SBRL': 'k'}
+mdict = {'CORELS': 's', 'C4.5': 'o', 'CART': 'd', 'RIPPER': '^', 'SBRL': 'v'}
 msdict = {'CORELS': 10, 'C4.5': ms, 'CART': ms, 'RIPPER': ms*2, 'SBRL': ms*2}
-mfcdict = {'CORELS': 'coral', 'C4.5': 'paleturquoise', 'CART': 'white', 'RIPPER': 'lightgray', 'SBRL': 'plum'}
-msvec = np.array([11, 9, 8, 10, 10, 9, 8, 7, 8, 6, 4]) * 2
-mew = 2
+mfcdict = {'CORELS': 'm', 'C4.5': 'c', 'CART': 'white', 'RIPPER': 'gray', 'SBRL': 'k'}
+msvec = np.array([6, 8, 10, 10, 4, 5, 6, 8, 4, 7, 10]) + 4
+mew = 1
 
+fs = 14
 ax1 = plt.subplot2grid((20, 60), (0, 1), colspan=44, rowspan=18)
 plt.xticks(fontsize=fs)
 plt.yticks(fontsize=fs)
@@ -53,14 +57,14 @@ plt.xlabel('Model size', fontsize=fs)
 plt.ylabel('Accuracy', fontsize=fs)
 ax1.set_xlim(0, 56)
 #ax2.set_ylim(0.64, 0.74)
-ax1.set_ylim(0.64, 0.92)
+ax1.set_ylim(0.64, 0.87)
 
 ax2 = plt.subplot2grid((20, 60), (0, 50), colspan=10, rowspan=18)
-plt.xticks([1100, 1400], fontsize=fs)
+plt.xticks([400, 700], fontsize=fs)
 plt.yticks(fontsize=fs)
 #plt.ylabel('Accuracy', fontsize=fs)
-ax2.set_xlim(1000, 1500)
-ax2.set_ylim(0.64, 0.92)
+ax2.set_xlim(320, 780)
+ax2.set_ylim(0.64, 0.87)
 
 i = 0
 for (method, xx, yy, w, h, ty, th) in data:
@@ -70,20 +74,34 @@ for (method, xx, yy, w, h, ty, th) in data:
         ax = ax2
     else:
         ax = ax1
-    if (w == 0):
-        if log2:
-            xx = np.log2(xx)
-        ax.plot(xx, yy, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc)
-        if (ax == ax2):
-            ax1.plot(xx, yy, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc)
-    else:
-        if log2:
-            xerr = np.array([[np.log2(xx) - np.log2(xx-w)], [np.log2(xx+w) - np.log2(xx)]])
-            xx = np.log2
-        else:
-            xerr = w
-        ax.errorbar(xx, yy, xerr=xerr, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc, capsize=4, elinewidth=2)
+    ax.plot(xx, yy, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc)
     i += 1
+
+legend = []
+for r in m[:-4]:
+    descr = r['Method']
+    if r['C']:
+        descr += ' (%s)' % ('%1.3f' % r['C']).strip('0')
+    elif r['cp']:
+        descr += ' (%s)' % ('%1.3f' % r['cp']).strip('0')
+    elif r['R']:
+        descr += ' (%s)' % ('%1.4f' % r['R']).strip('0')
+    legend += [descr]
+print legend
+ax1.legend(legend[:8], loc='upper left', fontsize=fs-3, numpoints=1, ncol=2, labelspacing=0.5, borderpad=0, columnspacing=0.1, markerscale=0.8, frameon=False)
+
+legend = []
+for r in m[-3:]:
+    descr = r['Method']
+    if r['C']:
+        descr += ' (%s)' % ('%1.5f' % r['C']).strip('0')
+    elif r['cp']:
+        descr += ' (%s)' % ('%1.3f' % r['cp']).strip('0')
+    elif r['R']:
+        descr += ' (%s)' % ('%1.4f' % r['R']).strip('0')
+    legend += [descr]
+print legend
+ax2.legend(legend, loc=(-2, 0.73), fontsize=fs-3, numpoints=1, ncol=1, labelspacing=0.5, borderpad=0, columnspacing=0.1, markerscale=0.8, frameon=False)
 
 i = 0
 for (method, xx, yy, w, h, ty, th) in data:
@@ -94,7 +112,7 @@ for (method, xx, yy, w, h, ty, th) in data:
         ax = ax1
     if log2:
         xx = np.log2(xx)
-    ax.errorbar(xx, yy, yerr=h, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc, capsize=4, elinewidth=2)
+    ax.errorbar(xx, yy, xerr=w, yerr=h, color=cdict[method], linewidth=0, marker=mdict[method], markersize=msvec[i], markeredgewidth=mew, markeredgecolor=cdict[method], markerfacecolor=mfc, capsize=0, elinewidth=1)
     i += 1
 
 if (with_training):
@@ -114,23 +132,8 @@ if (with_training):
                 xx = np.log2(xx)
             if (np.abs(ty - yy) > 0.01):
                 ax.plot([xx, xx], [ty, yy+h], ':', color=cdict[method], linewidth=2)
-            ax.plot(xx, ty, 'o', markersize=5, color='white', markeredgewidth=2, markeredgecolor='k')
+            ax.plot(xx, ty, 'o', markersize=5, color='white', markeredgewidth=mew, markeredgecolor='k')
         i += 1
-
-legend = []
-for r in m:
-    descr = r['Method']
-    if r['C']:
-        descr += ' (%s)' % ('%1.3f' % r['C']).strip('0')
-    elif r['cp']:
-        descr += ' (%s)' % ('%1.3f' % r['cp']).strip('0')
-    elif r['R']:
-        descr += ' (%s)' % ('%1.4f' % r['R']).strip('0')
-    legend += [descr]
-
-fs = 14
-
-ax1.legend(legend, loc='upper left', fontsize=fs-3, numpoints=1, ncol=3, labelspacing=0.5, borderpad=.5, columnspacing=0.1, markerscale=0.6)
 
 plt.suptitle('Weapon prediction (NYCLU stop-and-frisk dataset)', fontsize=fs)
 
