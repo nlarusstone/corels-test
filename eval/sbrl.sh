8c5ff834e6c1414cbf59adc256f788cf5199bec7
Elaine Angelino <elaine.angelino@gmail.com>
2017-06-09 15:55:29 -0700

    Working on adding some other SBRL points

--- eval/sbrl.sh
+++ eval/sbrl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ./sbrl.sh compas > compas_sparsity-sbrl-eta=15-lambda=5.txt
+# ./sbrl.sh frisk > frisk_sparsity-sbrl-eta=15-lambda=5.txt
+
 args=("$@")
 dataset=${args[0]}
 
@@ -9,5 +12,5 @@ do
     train_label=../data/CrossValidation/${dataset}_${i}_train.label
     test_out=../data/CrossValidation/${dataset}_${i}_test.out
     test_label=../data/CrossValidation/${dataset}_${i}_test.label
-    ./sbrlmod -t 3 -d 1 -S 0 $train_out $train_label $test_out $test_label
+    ./sbrlmod -t 3 -d 1 -e 15 -l 5 -i 10000 -S 0 $train_out $train_label $test_out $test_label
 done
