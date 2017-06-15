be49d436088dc593180b2a9a731a2d130e20b0b2
elaine84 <elaine.angelino@gmail.com>
2017-06-15 15:32:55 -0700

    Redo ablation figs for axis ticks

--- viz/kdd_compas_ablation.py
+++ viz/kdd_compas_ablation.py
@@ -72,7 +72,7 @@ make_figure = False
 num_folds = 1
 make_figure = True
 
-#make_small = False
+make_small = False
 make_small = True
 
 # log files generated on beepboop
@@ -257,13 +257,11 @@ for (ncomp, log_root) in enumerate(log_root_list):
             if (ncomp + 1 == ntot):
                 if make_small:
                     pylab.tight_layout()
-                    #pylab.legend(['%d' % ii for ii in range(1, 11)], loc=(-1.11, 1.77), handletextpad=0,
-                    #             borderaxespad=0.1, ncol=2, columnspacing=0.5, frameon=False)
-                    pylab.legend(['  %d' % ii for ii in range(1, 11)], loc=(-1.11, 1.75), handletextpad=0,
-                                 borderaxespad=0.1, ncol=2, columnspacing=1, frameon=False, fontsize=fs-3)
+                    pylab.legend(['%d' % ii for ii in range(1, 11)], loc=(-1.11, 1.77), handletextpad=0,
+                                 borderaxespad=0.1, ncol=2, columnspacing=0.5, frameon=False)
                 else:
                     pylab.suptitle('\nExecution traces of queue contents (ProPublica dataset)', fontsize=fs+2)
-                    pylab.legend(['%d' % ii for ii in range(1, 11)], loc=(-1.175, 2.93), handletextpad=0, labelspacing=0.3,  borderaxespad=0.1, ncol=2, columnspacing=0.5)
+                    pylab.legend(['%d' % ii for ii in range(1, 11)], loc=(-1.175, 2.93), handletextpad=0, labelspacing=0.3,  borderaxespad=0.1, ncol=2, columnspacing=0.5, frameon=False)
                 pylab.savefig('../figs/%s-queue.pdf' % ftag)
 
 max_prefix_length += 1
