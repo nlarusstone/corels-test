115560ddf8941a765a9a0531bea65fd39e26b49d
Elaine Angelino <elaine.angelino@gmail.com>
2017-07-18 15:33:37 -0700

    Update ablation script

--- eval/ablation.sh
+++ eval/ablation.sh
@@ -13,27 +13,27 @@ r=${args[3]}
 
 for i in `seq 0 9`;
 do
-    bbcache=./../src/bbcache
+    bbcache=./../src/corels
     out=../data/CrossValidation/${dataset}_${i}_train.out
     label=../data/CrossValidation/${dataset}_${i}_train.label
     minor=../data/CrossValidation/${dataset}_${i}_train.minor
     if [ "$ablation" = "none" ];
     then
-        $bbcache -n ${n} -c 2 -r $r -p 1 $out $label $minor &
+        $bbcache -n ${n} -v 10 -c 2 -r $r -p 1 $out $label $minor &
     elif [ "$ablation" = "priority" ];
     then
-        $bbcache -n ${n} -b -r $r -p 1 $out $label $minor &
+        $bbcache -n ${n} -v 10 -b -r $r -p 1 $out $label $minor &
     elif [ "$ablation" = "support" ];
     then
-        $bbcache -n ${n} -c 2 -r $r -p 1 -a 1 $out $label $minor &
+        $bbcache -n ${n} -v 10 -c 2 -r $r -p 1 -a 1 $out $label $minor &
     elif [ "$ablation" = "pmap" ];
     then
-        $bbcache -n ${n} -c 2 -r $r $out $label $minor &
+        $bbcache -n ${n} -v 10 -c 2 -r $r $out $label $minor &
     elif [ "$ablation" = "lookahead" ];
     then
-        $bbcache -n ${n} -c 2 -r $r -p 1 -a 2 $out $label $minor &
+        $bbcache -n ${n} -v 10 -c 2 -r $r -p 1 -a 2 $out $label $minor &
     elif [ "$ablation" = "identical" ];
     then
-        $bbcache -n ${n} -c 2 -r $r -p 1 $out $label
+        $bbcache -n ${n} -v 10 -c 2 -r $r -p 1 $out $label
     fi
 done
