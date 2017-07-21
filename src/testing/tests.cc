f744d48ac6f1b564baa1fe399fe9274c33b536e0
fingoldin <vassilioskaxiras@gmail.com>
2017-07-21 11:12:39 +0300

    Fix catch tree initialization

--- src/testing/tests.cc
+++ src/testing/tests.cc
@@ -13,23 +13,22 @@ extern int nsamples;
 
 TEST_CASE("Test prefix permutation map", "[prefixmap]") {
 
+    double c = 0.01;
     PrefixPermutationMap * pmap = new PrefixPermutationMap();
-    CacheTree * tree = new CacheTree(nsamples, nrules, 0.01, rules, labels, NULL, 0, false, "node");
+    CacheTree * tree = new CacheTree(nsamples, nrules, c, rules, labels, NULL, 0, false, "node");
 
     REQUIRE_FALSE(pmap == NULL);
     REQUIRE_FALSE(tree == NULL);
 
     SECTION("Insert into empty map") {
 
-        INFO("parent section");
+        //pmap->insert(1, nrules, 1, 1, 0.1, 0.12, tree->root(), 2, 5, 0, 0.01, 0.0, tree, NULL, tracking_vector<unsigned short, DataStruct::Tree>{});
+    }
 
-        SECTION("Insert with lower lb") {
-            INFO("first child section");
-        }
+    SECTION("Insert with lower lb") {
+    }
 
-        SECTION("Insert with high lb") {
-            INFO("second child section");
-        }
+    SECTION("Insert with high lb") {
     }
 
     if(pmap)
